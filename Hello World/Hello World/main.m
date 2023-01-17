38efac26dfa60501bbedc921c9b02ad579a1e17f
Sandeep Sahani <sandeepsahanigithub@gmail.com>
2023-01-17 10:11:22 +0530

    Initial commit

---
+++ Hello World/Hello World/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by Sandeep Sahani on 17/01/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool
+    {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
